85dc49ddbf640bb2611f253755ddaa823f3a3a49
benhmidamolka <150587946+benhmidamolka@users.noreply.github.com>
2026-08-25 13:15:22 +0100

    Delete fixmacity-backend/scratch directory

--- fixmacity-backend/scratch/test_schema.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const { pool } = require('../src/config/db');
-pool.query("SELECT column_name, data_type, udt_name FROM information_schema.columns WHERE table_name = 'declarations'")
-  .then(r => console.table(r.rows))
-  .finally(() => process.exit(0));
